92eb4e233d24c4eed148873eb7893c3e81d4a1e4
Toni Schönbuchner <toni.schoenbuchner@csgis.de>
2023-02-14 16:15:16 +0100

    granular templates and fixed config reader

--- config_reader.py
+++ config_reader.py
@@ -10,22 +10,24 @@ def read_config_from_json(resource_type: str) -> dict:
     :param resource_type: The type of resource for which the configuration is being read.
     :return: The JSON content of the configuration file for the specified resource type.
     """
+
     fields_file = os.path.join(
-        settings.PROJECT_ROOT,
-        settings.CUSTOM_METADATA_CONFIG_DIR,
+        os.path.dirname(os.path.dirname(__file__)),
+        "custom_metadata",
+        "metadata_fields",
         f"{resource_type}_fields.json",
     )
 
     if hasattr(settings, "CUSTOM_METADATA_CONFIG_DIR"):
         fields_file = os.path.join(
-            os.path.dirname(os.path.dirname(__file__)),
-            "metadata_fields",
+            settings.CUSTOM_METADATA_CONFIG_DIR,
             f"{resource_type}_fields.json",
         )
+
 
     try:
         with open(fields_file, "r") as f:
             json_file_content = json.load(f)
             return json_file_content
     except FileNotFoundError:
-        raise FileNotFoundError(f"Could not find the config file for {resource_type}.")
+        raise FileNotFoundError(f"Could not find the config file for {resource_type} in {fields_file}.")
